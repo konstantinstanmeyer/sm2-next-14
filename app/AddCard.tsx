561628110192578bcbe828f70bb67e4ba8804634
KonstantinStanmeyer <konstantin@stanmeyer.com>
2025-02-10 10:10:48 -0500

    crossed-out model draw button on AddCard

--- app/AddCard.tsx
+++ app/AddCard.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import { useState } from "react";
+import PixelCanvas from "./PixelCanvas";
 
 interface Props {
     language: string;
@@ -5,12 +9,15 @@ interface Props {
 }
 
 export default function AddCard({ language, text }: Props){
-    
+    const [isDrawing, setIsDrawing] = useState<boolean>(false);
 
     return (
-        <div className="">
-            <form>
-                <textarea className="h-fit w-30 bg-[#ffe8ce] border-[1px] border-black resize-none px-4 py-3" value={text} />
+        <div className="pixelify flex flex-col items-center">
+            <h2 className="text-2xl">Add Card</h2>
+            <form className="my-2 flex flex-col items-center">
+                <textarea className="h-fit w-30 bg-[#ffe8ce] border-[1px] rounded-[10px] border-black resize-none px-4 py-3" value={text} />
+                <p className={`${isDrawing ? "" : "crossed-out"} cursor-pointer`} onClick={() => setIsDrawing(isDrawing => !isDrawing)}>draw</p>
+                {isDrawing ? <PixelCanvas/> : null}
             </form>
         </div>
     )
